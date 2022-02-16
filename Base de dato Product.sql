d166f5d6a3df4c9ebe3acd2fe1e9048b5fc1f2f0
Juan Bautista Correa <jbautistacorrea@gmail.com>
2022-02-16 20:55:16 -0300

    Commit inicial para archivos del proyecto

---
+++ Base de dato Product.sql
@@ -0,0 +1,25 @@
+CREATE TABLE mytable(
+   id_product     VARCHAR(36) NOT NULL PRIMARY KEY
+  ,dv_edit        VARCHAR(4)
+  ,dv_load        VARCHAR(19) NOT NULL
+  ,dv_name        VARCHAR(12) NOT NULL
+  ,dv_portion     INTEGER  NOT NULL
+  ,dv_price       INTEGER  NOT NULL
+  ,dv_status      BIT  NOT NULL
+  ,dv_stock       INTEGER  NOT NULL
+  ,dv_type        VARCHAR(8) NOT NULL
+  ,photo_id_photo VARCHAR(36) NOT NULL
+  ,ticket         VARCHAR(4)
+);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('20b7c746-cac5-43ff-968f-62c1b06efbe1',NULL,'2021-09-28 00:12:25','bondiola',8,250,1,50,'celiaca','58f97fac-c895-47cf-a7ad-48c55a764ef0',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('3ff79861-d70a-4db1-8309-a12d1d4b5466',NULL,'2021-09-28 00:08:37','caprese',8,250,1,50,'vegana','3759ace3-6ae9-4816-99a0-53effa569f45',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('49c79495-a2ab-42c3-9923-236e7b6fe33f',NULL,'2021-09-28 00:09:23','muzarella',8,250,1,50,'vegana','6251adf5-c922-4618-861f-447c7a57623d',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('56d79a3a-7f7a-4a62-b1a4-3e59b1704053',NULL,'2021-09-28 00:12:46','champiñones',8,250,1,50,'celiaca','4f7d6a17-c0c6-49ba-a4fa-316d43072edd',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('5a3b7f10-4b06-4f28-b435-70938ca7e3c6',NULL,'2021-09-28 00:11:07','calabresa',8,250,1,50,'clasicas','9019c53b-e59c-466e-877a-e9b8f340f156',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('5d65f008-1db8-4ff0-9bfb-1dfa2b1933a0',NULL,'2021-09-28 00:10:46','anchoas',8,250,1,50,'clasicas','e12a49b6-346a-41ea-a588-c929abb7b340',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('6250158f-5138-4a7c-b756-ac1a98f88c7b',NULL,'2021-09-28 00:10:02','rucula',8,250,1,50,'vegana','2becbb0b-876d-4da0-9b0f-7525a2ef09f8',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('65fa7ceb-302d-45b5-b4f1-bc701efc4121',NULL,'2021-09-28 00:13:28','cuatroquesos',8,250,1,50,'celiaca','83ffd5a0-ec73-4600-a113-6761bd324af7',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('6a16cf93-9026-4758-ab18-3bc6c82fc763',NULL,'2021-09-28 00:11:24','especial',8,250,1,50,'clasicas','010a190c-f8de-49d7-8fe4-0cddeecd0a1d',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('a22c7b93-863c-4eaa-869b-a23bd0a5fcce',NULL,'2021-09-28 00:11:59','serrana',8,250,1,50,'clasicas','08d38cd2-d1e4-4b31-9a37-aea49594cccc',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('bf8f3a8e-2c9c-43cc-9124-6e7cea0beb6b',NULL,'2021-09-28 00:13:56','fugazzeta',8,250,1,50,'celiaca','8f623e65-c059-46c0-ba13-7b5ef035797e',NULL);
+INSERT INTO mytable(id_product,dv_edit,dv_load,dv_name,dv_portion,dv_price,dv_status,dv_stock,dv_type,photo_id_photo,ticket) VALUES ('ff39b0bc-a439-498e-a948-b0a09fbbf98b',NULL,'2021-09-28 00:09:44','napolitana',8,250,1,50,'vegana','3f9ddd9a-1bda-4a1c-8809-d09259405314',NULL);
